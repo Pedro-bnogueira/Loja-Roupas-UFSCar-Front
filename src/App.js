b54b95bafe4c124092d2dcfd8a0b314da1250431
Pedro-bnogueira <pebnogueira@gmail.com>
2024-11-26 20:58:07 -0300

    atualização de rotas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import AdminRegister from './components/AdminRegister';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/adm/new/user' element={<AdminRegister />}/>
+        <Route path='/' exact element={<Home />}>
+          
+        </Route>
+	
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
